b84227da4de5658bd249f285a9c6afb7269d4a80
luyigen <luyigen@empowerwin.com>
2022-02-16 17:01:08 +0800

    babel添加缓存文件夹选项

--- webpack.config.js
+++ webpack.config.js
@@ -30,14 +30,16 @@ const config = {
     module: {
         rules: [
             {
-              test: /\.m?js$/,
-              exclude: /(node_modules|bower_components)/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['@babel/preset-env']
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    cacheDirectory: true,
+                    presets: ['@babel/preset-env'],
+                    plugins: ['@babel/plugin-proposal-object-rest-spread']
+                  }
                 }
-              }
             }
         ],
     },
