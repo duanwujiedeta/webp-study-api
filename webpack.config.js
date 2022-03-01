e172e0680f530b8f2ec9c0991f79b74a152efc0a
luyigen <luyigen@empowerwin.com>
2022-03-01 17:36:47 +0800

    optimization.moduleIds，用来配置模块id

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,36 @@
 const webpack = require('webpack');
 var path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-module.exports = {
+module.exports = [
+	{
+        mode: "development" || "production",
+		entry: "./moduleIds/example.js",
+		output: {
+			pathinfo: true,
+			filename: "output.js",
+            clean: true
+		},
+		optimization: {
+			moduleIds: "size",
+			usedExports: true,
+			mangleExports: true
+		}
+	},
+	{
+        mode: "development" || "production",
+		entry: "./moduleIds/example.js",
+		output: {
+			pathinfo: true,
+			filename: "without.js",
+            clean: true
+		},
+		optimization: {
+			moduleIds: "size",
+			usedExports: false,
+			mangleExports: false
+		}
+	}
+]; /* {
     mode: "development" || "production",
     entry: {
         main: "./runtimeChunk/example"
@@ -20,4 +49,4 @@ module.exports = {
     optimization: {
         runtimeChunk: true
     }
-};
+}; */
