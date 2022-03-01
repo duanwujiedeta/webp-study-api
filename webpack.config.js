040cdacef014147f1671b0f36e62b33a469300d1
luyigen <luyigen@empowerwin.com>
2022-03-01 17:13:23 +0800

    添加optimization.chunkIds，并且以这个id连进行chunk文件的相应命名，可以保证不同模式之间的文件名一致

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,27 @@
 const webpack = require('webpack');
 var path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-process.env.NODE_ENV1 = "111111111111111";
-process.env.DEBUG = "222222222222";
-// const ESLintPlugin = require('eslint-webpack-plugin');
 module.exports = {
-    mode: "development",
-    entry: {
-        "index": "./src/index.js"
-    },
-    output: {
-        path: path.join(__dirname, "dist"),
-        filename: "MyDll.[name].js",
-        library: "[name]_[fullhash]",
-        clean: true
-    },
-    plugins: [
-        /* new ESLintPlugin({
-            extensions: ['js']
-        }), */
+	mode: "development" || "production",
+	entry: {
+		pageA: "./deterministic/pageA",
+		pageB: "./deterministic/pageB",
+		pageC: "./deterministic/pageC"
+	},
+	output: {
+		path: path.join(__dirname, "dist"),
+		filename: "[name].bundle.js",
+		chunkFilename: "[id].chunk.js"
+	},
+	plugins: [
         new HtmlWebpackPlugin({
             template: 'index.html',
         }),
-        new webpack.EnvironmentPlugin(['NODE_ENV1', 'DEBUG'])
-        /* new webpack.EnvironmentPlugin({
-            NODE_ENV1: 'development111',
-            DEBUG: 'DEBUG111',
-        }) */
-        /**
-         上面的效果相当于
-         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-            'process.env.DEBUG': JSON.stringify(process.env.DEBUG),
-         });
-         */
-    ]
+		new webpack.optimize.AggressiveMergingPlugin({
+			minSizeReduce: 1.5
+		})
+	],
+	optimization: {
+		chunkIds: "deterministic" // To keep filename consistent between different modes (for example building only)
+	}
 };
