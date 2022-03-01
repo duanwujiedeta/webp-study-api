9ff824d8b154043b69cb1cd8595d498051abfbb2
luyigen <luyigen@empowerwin.com>
2022-03-01 17:26:18 +0800

    生成含有 runtime.js 对应的单独公共文件

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,22 @@ const webpack = require('webpack');
 var path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-	mode: "development" || "production",
-	entry: {
-		pageA: "./deterministic/pageA",
-		pageB: "./deterministic/pageB",
-		pageC: "./deterministic/pageC"
-	},
-	output: {
-		path: path.join(__dirname, "dist"),
-		filename: "[name].bundle.js",
-		chunkFilename: "[id].chunk.js"
-	},
-	plugins: [
+    mode: "development" || "production",
+    entry: {
+        main: "./runtimeChunk/example"
+    },
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "[name].chunkhash.js",
+        chunkFilename: "[name].chunkhash.js",
+        clean: true
+    },
+    plugins: [
         new HtmlWebpackPlugin({
             template: 'index.html',
-        }),
-		new webpack.optimize.AggressiveMergingPlugin({
-			minSizeReduce: 1.5
-		})
-	],
-	optimization: {
-		chunkIds: "deterministic" // To keep filename consistent between different modes (for example building only)
-	}
+        })
+    ],
+    optimization: {
+        runtimeChunk: true
+    }
 };
